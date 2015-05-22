71fef4c1b271500acdd3c9d5a2543f5c674b3a0a
Alexandre Possebom <alexandrepossebom@gmail.com>
2015-05-22 12:58:49 -0300

    first commit

---
+++ app/src/main/java/com/possebom/fixmobileradiobug/FixMobileRadioAlarm.java
@@ -0,0 +1,59 @@
+package com.possebom.fixmobileradiobug;
+
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.os.Build;
+
+import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.XposedBridge;
+import de.robv.android.xposed.XposedHelpers;
+import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
+
+/**
+ * Created by alexandre on 22/05/15.
+ */
+public class FixMobileRadioAlarm implements IXposedHookLoadPackage {
+    private long beforeAlarmSchedule;
+
+    private static final String CLASS_NAME = "com.android.server.content.SyncManager$SyncHandler";
+
+    @Override
+    public void handleLoadPackage(LoadPackageParam lpparam) throws Throwable {
+
+        if (Build.VERSION.SDK_INT != Build.VERSION_CODES.LOLLIPOP) {
+            XposedBridge.log("module disabled. This will only work with Android 5.0.x!");
+            return;
+        }
+
+        XposedHelpers.findAndHookMethod(CLASS_NAME, lpparam.classLoader,
+                "manageSyncAlarmLocked", "long", "long", new XC_MethodHook() {
+                    @Override
+                    protected void beforeHookedMethod(XC_MethodHook.MethodHookParam param) throws Throwable {
+                        beforeAlarmSchedule = XposedHelpers.getLongField(param.thisObject, "mAlarmScheduleTime");
+                        XposedBridge.log("SyncManager before");
+                    }
+
+                    @Override
+                    protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                        XposedBridge.log("SyncManager after");
+                        boolean shoudSet = XposedHelpers.getBooleanField(param.thisObject, "shoudSet");
+                        boolean needAlarm = XposedHelpers.getBooleanField(param.thisObject, "needAlarm");
+
+                        if (shoudSet && needAlarm) {
+                            final boolean alarmIsActive = XposedHelpers.getBooleanField(param.thisObject, "alarmIsActive");
+                            final long alarmTime = XposedHelpers.getLongField(param.thisObject, "alarmTime");
+
+                            if (alarmIsActive && alarmTime <= beforeAlarmSchedule) {
+                                final PendingIntent mSyncAlarmIntent = (PendingIntent) XposedHelpers.getObjectField(param.thisObject, "PendingIntent");
+                                final AlarmManager mAlarmService = (AlarmManager) XposedHelpers.getObjectField(param.thisObject, "mAlarmService");
+                                mAlarmService.cancel(mSyncAlarmIntent);
+                                XposedHelpers.setObjectField(param.thisObject, "mAlarmScheduleTime", null);
+                                XposedBridge.log("Alarm is canceled");
+                            }
+                        }
+
+                    }
+                });
+    }
+}
